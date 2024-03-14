97b65063c6d6ebcdf470378ecfed5e79cca0f5fa
David Rempel <ddrempti@gmail.com>
2024-03-14 08:05:17 -0600

    Changes

--- keyboards/keebio/iris/keymaps/dtremp007/keymap.c
+++ keyboards/keebio/iris/keymaps/dtremp007/keymap.c
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
       KC_CAPS_LOCK, LCTL_T(KC_A), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T), KC_D,         KC_H, RSFT_T(KC_N), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_O), KC_QUOT,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-           KC_LEFT_CTRL, LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_SPC,             TG(_LOWER),  KC_K, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), XXXXXXX,
+           LCTL(KC_B), LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_ESC,             TG(_LOWER),  KC_K, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), XXXXXXX,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                         KC_ESCAPE, LT(_RAISE, KC_BSPC), LT(_ADJUST, KC_TAB),        KC_ENT, LT(_LOWER, KC_SPC), KC_COLON
+                         KC_SPC, LT(_RAISE, KC_BSPC), LT(_ADJUST, KC_TAB),        KC_ENT, LT(_LOWER, KC_SPC), KC_COLON
         //                                    └────────┴────────┴────────┘         └────────┴────────┴────────┘
         ),
 
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
            QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                               KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-           _______, _______, KC_P4,   KC_P5,   KC_P9, KC_PLUS,                           KC_DOWN, KC_RGHT, KC_MPLY, KC_VOLD, KC_VOLU, _______,
+           QK_BACKLIGHT_OFF, _______, KC_P7,   KC_P8,   KC_P9, KC_PLUS,                           KC_DOWN, KC_RGHT, KC_MPLY, KC_VOLD, KC_VOLU, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        _______, KC_LEFT_CTRL, LALT_T(KC_P1), LGUI_T(KC_P2), LSFT_T(KC_P3), KC_MINS,          KC_LEFT, KC_MINS, KC_EQL, _______, _______, _______,
+        _______, KC_LEFT_CTRL, LALT_T(KC_P4), LGUI_T(KC_P5), LSFT_T(KC_P6), KC_MINS,          KC_LEFT, KC_MINS, KC_EQL, _______, _______, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-            _______, KC_P0, KC_P8, KC_P6, KC_P7, KC_PDOT, _______,              _______, KC_UP, KC_LBRC, KC_RBRC, _______, _______, _______,
+            _______, KC_P0, KC_P1, KC_P2, KC_P3, KC_PDOT, _______,              _______, KC_UP, KC_LBRC, KC_RBRC, _______, _______, _______,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                              _______, _______, _______,                  _______, MO(_ADJUST), _______
         //                                 └────────┴────────┴────────┘                 └────────┴────────┴────────┘
